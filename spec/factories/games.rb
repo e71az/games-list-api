fc23438c21f807673ac3356bd14d175dadf45fcf
Elias Castañeda <e71az@icloud.com>
2021-05-18 09:27:35 -0600

    Add: More TDD for the game model

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     id 1
     name { Faker::Lorem.word }
     description { Faker::Lorem.words(10) }
-    image { Faker::Lorem.word(50) }
+    image { Faker::Image.imageUrl(50, 50, "games") }
     price { Faker::Number.number(99) }
   end
 end
