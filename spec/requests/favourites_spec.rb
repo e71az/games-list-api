570805abb206494eb1bf23184e3a5b958c182e1c
Elias Castañeda <e71az@icloud.com>
2021-06-01 06:44:11 -0600

    Fix: Linting errors

--- spec/requests/favourites_spec.rb
+++ spec/requests/favourites_spec.rb
@@ -1,17 +1,17 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Favourites", type: :request do
+RSpec.describe 'Favourites', type: :request do
   let!(:favourites) { create_list(:favourite, 3) }
 
-  describe "GET /favourites" do
-    before { get "/favourites" }
+  describe 'GET /favourites' do
+    before { get '/favourites' }
 
-    it "returns favourites" do
+    it 'returns favourites' do
       expect(json).not_to be_empty
       expect(json.size).to eq(3)
     end
 
-    it "returns status code 200" do
+    it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
   end
