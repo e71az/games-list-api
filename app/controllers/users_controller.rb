0494f8b74a57b0fa3f980af8d21f2ee3ae6d8f72
Elias Castañeda <e71az@icloud.com>
2021-06-08 09:46:11 -0600

    Delete: Sessions controller and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def create
     @user = User.create!(user_params)
-    session[:user_id] = @user.id
     json_response(@user, :created)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get "sessions/create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :games do
     put :favourite, on: :member
   end
   resources :users, only: [:new, :create]
   resources :favourites, only: [:index, :create, :destroy]
-
-  post "login", to: "sessions#create"
 end
